bed629fd7f50f5422efa0a63eaaddf4cb873199c
Eni Magdalena Oreč <eo54656@fer.hr>
2024-10-30 17:11:15 +0100

    Finish Login and Register design.

--- Source/Frontend/task-mates/src/app/login/login.component.ts
+++ Source/Frontend/task-mates/src/app/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Standalone forms
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule, RouterModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string | undefined;
+  password: string | undefined;
+  errorMessage: string | undefined;
+  loginForm!: FormGroup;
 
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      Email: new FormControl('', Validators.required),
+      Password: new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit() {
+    this.errorMessage = undefined;
+    // Call API to login
+  }
 }
