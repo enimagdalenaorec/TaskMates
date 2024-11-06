89f98a71d09c5bbd8d115442f6d74c329228fba6
enadvojak <ena.dvojak@fer.unizg.hr>
2024-11-06 10:04:27 +0100

    initial commit

--- Source/Frontend/task-mates/src/app/create-group/create-group.component.ts
+++ Source/Frontend/task-mates/src/app/create-group/create-group.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Standalone forms
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-create-group',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, InputTextModule, ButtonModule, RouterModule, NgIf],
   templateUrl: './create-group.component.html',
   styleUrl: './create-group.component.css'
 })
 export class CreateGroupComponent {
+  groupName: string | undefined;
+  errorMessage: string | undefined;
+  createGroupForm!: FormGroup;
 
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.createGroupForm = new FormGroup({
+      GroupName: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    this.errorMessage = undefined;
+    // Call API to login
+  }
 }
