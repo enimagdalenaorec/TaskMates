f7f75358538c8c7a0a7b85b4b41cf5c163618dba
Lovre565 <marasovic.lovre@gmail.com>
2024-11-08 16:43:50 +0100

    Attempt to google AOuth with django

--- Source/Backend/myproject/settings.py
+++ Source/Backend/myproject/settings.py
@@ -155,5 +155,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS=("django.contrib.auth.backends.ModelBackend","allauth.account.auth_backends.AuthenticationBackend")
 
-LOGIN_REDIRECT_URL="/"
-LOGOUT_REDIRECT_URL="/"
+LOGIN_REDIRECT_URL="http://localhost:4200/my-groups"
+LOGOUT_REDIRECT_URL="http://localhost:4200/"
