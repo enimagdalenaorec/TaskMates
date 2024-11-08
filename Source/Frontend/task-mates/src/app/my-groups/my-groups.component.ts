da03d8d73f03064b14187cd9a735341b10f3fadd
Ana Gasperov <ana.gasperov2@gmail.com>
2024-11-08 18:11:22 +0100

    create my groups

--- Source/Frontend/task-mates/src/app/my-groups/my-groups.component.ts
+++ Source/Frontend/task-mates/src/app/my-groups/my-groups.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { BadgeModule } from 'primeng/badge';
+
 
 @Component({
   selector: 'app-my-groups',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, InputTextModule, ButtonModule, CardModule, BadgeModule],
   templateUrl: './my-groups.component.html',
   styleUrl: './my-groups.component.css'
 })
 export class MyGroupsComponent {
 
+  searchQuery='';
+  groups = [
+    { name: 'Roommates', hasNotification: true, hasChat: true },
+    { name: 'Biology project', hasNotification: false, hasChat: false },
+    { name: 'Parents house', hasNotification: false, hasChat: true },
+    // Add more groups as needed
+  ];
 }
