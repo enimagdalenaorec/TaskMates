910ff4712c61b183f4f8b7d3540d51ccbc6f9eeb
avsprljan <as54994@fer.hr>
2024-11-14 16:57:29 +0100

    Change localhost

--- Source/Backend/myproject/settings.py
+++ Source/Backend/myproject/settings.py
@@ -174,5 +174,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS=("django.contrib.auth.backends.ModelBackend","allauth.account.auth_backends.AuthenticationBackend")
 
-LOGIN_REDIRECT_URL="http://localhost:4200/my-groups"
-LOGOUT_REDIRECT_URL="http://localhost:4200/my-groups"
+LOGIN_REDIRECT_URL="https://taskmatesbackend-pd5h.onrender.com/my-groups"
+LOGOUT_REDIRECT_URL="https://taskmatesbackend-pd5h.onrender.com/my-groups"
