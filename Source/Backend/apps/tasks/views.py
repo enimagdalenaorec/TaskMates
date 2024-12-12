06e67bc18b5ea9d9149fe5fe4a0eee55c87ae3fc
Eni Magdalena Oreč <eo54656@fer.hr>
2024-12-12 21:00:18 +0100

    Display part of tasks.

--- Source/Backend/apps/tasks/views.py
+++ Source/Backend/apps/tasks/views.py
@@ -7,9 +7,9 @@ from rest_framework.status import HTTP_200_OK
 def mock_get_tasks_by_group(request):
     return Response({
         "tasks": [
-        {"id": "1", "name": "Task1", "description": "description1", "points": "300", "maxCapacity": "3", "status": "full", "picture": "", "icon": None, "deadline": "2023-12-31", "members": [{"name": "Member1"}], "groupId": 1, "groupName": "Group1"},
-        {"id": "2", "name": "Task2", "description": "description2", "points": "400", "maxCapacity": "2", "status": "available", "picture": "", "icon": None, "deadline": "2023-12-31", "members": [{"name": "Member2"}], "groupId": 1, "groupName": "Group1"},
-        {"id": "3", "name": "Task3", "description": "description3", "points": "500", "maxCapacity": "4", "status": "finished", "picture": "", "icon": None, "deadline": "2023-11-30", "members": [{"name": "Member3"}], "groupId": 2, "groupName": "Group2"},
+        {"id": "1", "name": "Task1", "description": "description1", "points": "300", "maxCapacity": "3", "status": "available", "picture": "", "icon": None, "deadline": "2024-12-31", "members": [{"name": "Member1"}, {"name": "Member2"}], "groupId": 1, "groupName": "Group1"},
+        {"id": "2", "name": "Task2", "description": "description2", "points": "400", "maxCapacity": "2", "status": "full", "picture": "", "icon": None, "deadline": "2024-12-31", "members": [{"name": "Member2"}, {"name": "Member3"}], "groupId": 1, "groupName": "Group1"},
+        {"id": "3", "name": "Task3", "description": "description3", "points": "500", "maxCapacity": "4", "status": "finished", "picture": "", "icon": None, "deadline": "2023-11-30", "members": [{"name": "Member3"}, {"name": "Member2"}, {"name": "Member3"}], "groupId": 2, "groupName": "Group2"},
         {"id": "4", "name": "Task4", "description": "description4", "points": "600", "maxCapacity": "5", "status": "failed", "picture": "", "icon": None, "deadline": "2023-10-31", "members": [{"name": "Member4"}], "groupId": 2, "groupName": "Group2"}
         ]
     }, status=HTTP_200_OK)
