f65a3560c99c51fff6829f8f1a0297625eee9568
Eni Magdalena Oreč <eo54656@fer.hr>
2024-12-15 16:09:32 +0100

    Finish Add Task Form.

--- Source/Frontend/task-mates/src/app/add-task/add-task.component.ts
+++ Source/Frontend/task-mates/src/app/add-task/add-task.component.ts
@@ -7,25 +7,34 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { SliderModule } from 'primeng/slider';
 import { CalendarModule } from 'primeng/calendar';
 import { ActivatedRoute } from '@angular/router';
+import { PickerModule } from "@ctrl/ngx-emoji-mart";
+import { NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [ReactiveFormsModule, InputTextModule, InputTextareaModule, InputNumberModule, SliderModule, CalendarModule],
+  imports: [ReactiveFormsModule, InputTextModule, InputTextareaModule, InputNumberModule, SliderModule, CalendarModule, PickerModule, NgIf, HttpClientModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent implements OnInit {
   addTaskForm: FormGroup;
   groupId: string = '';
+  showEmojiPicker = false;
+  message = '';
+  icon = '';
+  apiUrl = 'http://127.0.0.1:8000/api'; // Django API endpoints
+  today: Date = new Date();
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.addTaskForm = new FormGroup({
       name: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required),
       capacity: new FormControl(1, Validators.required),
       points: new FormControl(100, Validators.required),
-      deadline: new FormControl(new Date(), Validators.required)
+      deadline: new FormControl(new Date(), Validators.required),
+      // icon: new FormControl('', Validators.required)
     });
   }
 
@@ -34,8 +43,60 @@ export class AddTaskComponent implements OnInit {
       this.addTaskForm.get('points')?.setValue(value, { emitEvent: false });
     });
     this.groupId = this.route.snapshot.paramMap.get('group-id')!;
-    console.log('Group ID:', this.groupId);
+    // console.log('Group ID:', this.groupId);
   }
 
-  onSubmit() {}
+  addEmoji(event: any) {
+    if (this.icon === '') {
+      const { message } = this;
+      this.icon = `${message}${event.emoji.native}`;
+      this.showEmojiPicker = false;
+    }
+  }
+
+  toggleVisability() {
+    if (this.icon === '') {
+    this.showEmojiPicker = !this.showEmojiPicker;
+    }
+  }
+
+  handleKeydown(event: KeyboardEvent): void {
+    if (event.key !== 'Backspace' && event.key !== 'Delete') {
+      event.preventDefault();
+    } else {
+      this.icon = '';
+    }
+  }
+
+  isFormDisabled(): boolean {
+    return this.addTaskForm.invalid || this.icon === '';
+  }
+
+  onSubmit() {
+    const body = {
+      name: this.addTaskForm.value.name,
+      description: this.addTaskForm.value.description,
+      capacity: this.addTaskForm.value.capacity,
+      points: this.addTaskForm.value.points,
+      deadline: this.addTaskForm.value.deadline.getFullYear() + '-' + this.increaseMonth(this.addTaskForm.value.deadline.getMonth()) + '-' + this.addTaskForm.value.deadline.getDate(),
+      icon: this.icon,
+      groupId: this.groupId
+    };
+    // console.log('Task:', body);
+    this.http.post(this.apiUrl + '/tasks/addTask', body).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error('Error adding task:', error);
+      }
+    });
+    this.addTaskForm.reset();
+    this.addTaskForm.patchValue({ capacity: 1, points: 100, deadline: new Date() });
+    this.icon = '';
+  }
+
+  increaseMonth(number: number): number {     //not sure why i had to do this?
+    return number + 1;
+  }
 }
