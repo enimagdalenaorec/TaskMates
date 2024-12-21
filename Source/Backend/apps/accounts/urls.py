08d2691720c2ea46215923270e66f7f64a5f918f
Lovre565 <marasovic.lovre@gmail.com>
2024-12-21 11:47:31 +0100

    accounts urls fix

--- Source/Backend/apps/accounts/urls.py
+++ Source/Backend/apps/accounts/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.mock_register, name='mock_register'),
-    path('login', views.mock_login, name='mock_login'),
     path('',views.home),
     path('logout',views.logout_view),
     path('googleLogin/', views.homere, name='homere'),
