926e207d55fb5f6c022f104794a0d5529a37417a
Lovre565 <marasovic.lovre@gmail.com>
2025-01-12 15:39:26 +0100

    Fix calendar include

--- Source/Backend/apps/calendar/views.py
+++ Source/Backend/apps/calendar/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
-from core.models import Task
+from core.models import Task, UserTask
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])  # Korisnik mora biti autentificiran
