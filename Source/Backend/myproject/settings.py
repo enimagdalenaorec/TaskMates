13aed0c655831e47f13f97532d4b3389bf6432e7
avsprljan <as54994@fer.hr>
2025-01-23 15:02:00 +0100

    nez zasto je ovo potrebno

--- Source/Backend/myproject/settings.py
+++ Source/Backend/myproject/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'oauth2.provider',
     'apps.calendar',
     'apps.groups',
     'apps.notifications',
