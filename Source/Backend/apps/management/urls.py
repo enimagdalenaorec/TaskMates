b579346c6355a96717b4953d2c6153ededbd3111
avsprljan <as54994@fer.hr>
2025-01-24 17:51:50 +0100

    test 1

--- Source/Backend/apps/management/urls.py
+++ Source/Backend/apps/management/urls.py
@@ -6,5 +6,5 @@ from . import views
 
 urlpatterns = [
     path('run_check_deadlines/', run_check_deadlines, name='run_check_deadlines'),
-
+    path('test_csrf/', views.test_csrf, name='test_csrf')
 ]
--- Source/Backend/apps/management/views.py
+++ Source/Backend/apps/management/views.py
@@ -10,3 +10,14 @@ def run_check_deadlines(request):
     except Exception as e:
         return JsonResponse({"status": "error", "message": str(e)})
 
+from django.http import HttpResponse
+from django.middleware.csrf import get_token
+
+def test_csrf(request):
+    csrf_token = get_token(request)
+    print(f"CSRF token: {csrf_token}")  # Print the generated CSRF token
+
+    response = HttpResponse("CSRF test")
+    response["Access-Control-Allow-Origin"] = "https://taskmates-gjhi.onrender.com"  # Important for testing
+    response["Access-Control-Allow-Credentials"] = "true"  # Important for testing
+    return response
